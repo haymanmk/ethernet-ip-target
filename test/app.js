6d1676d11048105faa5a81c2f1a01f0b9b33de2a
haymanmk <haymanmk@gmail.com>
2023-03-07 18:21:31 +0800

    prototype of class 1 IO messaging

--- test/app.js
+++ test/app.js
@@ -4,8 +4,10 @@ const { generateSessionID } = require("../src/server/utils");
 const {
   registerSessionReply,
   CPF,
+  UDPDatagram,
 } = require("../src/server/enip/encapsulation");
 const { MessageRouter, ForwardOpen } = require("../src/server/enip/cip");
+const { UDPServer } = require("../src/server/udp");
 
 const enip = new ENIP();
 enip.createListener();
@@ -16,22 +18,52 @@ enip.on("Register Session Request", (data) => {
   enip.socket.write(registerSessionReply(enip.state.session.id, data));
 });
 
+const udpServer = new UDPServer();
+let sequenceNumber = 0;
+let sequenceCount = 0;
+
+udpServer.on("UDP message received", (msg, rinfo) => {
+  console.log(`UDP message received`);
+  let parsedResult = UDPDatagram.parse(
+    msg,
+    1,
+    UDPDatagram.realTimeFormats.MODELESS
+  );
+  console.log(`Got data: `);
+  console.log(parsedResult.data);
+
+  //create foo data
+  let fooData = Buffer.from([0x02, 0x13]);
+  sequenceNumber++;
+  sequenceCount++;
+  if (sequenceNumber > Math.pow(2, 32) - 1) sequenceNumber = 0;
+  if (sequenceCount > Math.pow(2, 16) - 1) sequenceCount = 0;
+
+  const packet = UDPDatagram.build(
+    forwardOpenRequest.t_o_network_connection_id,
+    sequenceNumber,
+    fooData,
+    1,
+    sequenceCount
+  );
+
+  udpServer.sendData(packet, 2222, "172.16.0.10");
+});
+
+let forwardOpenRequest;
 enip.on("SendRRData Request Received", (data, timeout) => {
-  const server = dgram.createSocket("udp4");
-
-  server.on("message", (msg, rinfo) => {
-    console.log(`Server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  });
-  server.on("listening", () => {
-    const address = server.address();
-
-    console.log(`Server listening ${address.address}:${address.port}`);
-  });
-  server.bind(2222, "172.16.0.24");
-
+  if (udpServer) {
+    try {
+      udpServer.bindPort(2222);
+    } catch (err) {
+      console.log(`Error occurred when binding UDP socket: ${err}`);
+      // if (!err.includes("ERR_SOCKET_ALREADY_BOUND"))
+      //   throw new Error(`Error occurred when binding UDP socket: ${err}`);
+    }
+  }
   const MRData = data[1]; //the first item in data arry is the Address Item.
   const parsedMRData = MessageRouter.parse(MRData.data);
-  const forwardOpenRequest = ForwardOpen.parse(parsedMRData.request_data);
+  forwardOpenRequest = ForwardOpen.parse(parsedMRData.request_data);
   const forwardOpenReply = ForwardOpen.build(forwardOpenRequest);
 
   let sockaddrArray = null;
@@ -52,3 +84,5 @@ enip.on("SendRRData Request Received", (data, timeout) => {
 enip.on("Unhandled Encapsulated Command Received", (data) => {
   console.log(`Unhandled Data Received: ${data.commandCode}`);
 });
+
+function createIOData(data) {}
